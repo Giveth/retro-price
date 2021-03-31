390dc3053ca88d403fd5befb8e07a3b8d2d9bc2e
James Farrell <info@jamespfarrell.com>
2021-03-31 12:34:05 +0200

    working with calling the graph and getting buy/ sell

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const { Client } = require('pg')
+const { fetchSwapForPair } = require('./theGraph')
+
 require('dotenv').config()
 const client = new Client({
   user: process.env.DB_USER,
@@ -7,15 +9,30 @@ const client = new Client({
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
 })
+const wethUsdcPair = "0xb4e16d0168e52d35cacd2c6185b44281ec28c9dc"
+
 async function run() {
   await client.connect()
   //const res = await client.query('SELECT $1::text as message', ['Hello world!'])
   //const res = await client.query('SELECT NOW()')
   const res = await client.query('select d."createdAt" , EXTRACT(EPOCH from d."createdAt") as "donatedTime" from donation d where d."priceEth" is null')
   console.log(`res : ${JSON.stringify(res, null, 2)}`)
-  res.rows.forEach(row => {
+  res.rows.forEach(async row => {
     console.log(`row.createdAt ---> : ${row.createdAt} - ${row.donatedTime}`)
-
+    const swaps = await fetchSwapForPair(wethUsdcPair, Math.round(row.donatedTime))
+    console.log(`swaps : ${JSON.stringify(swaps, null, 2)}`)
+    
+    let action = ''
+    if(Number(swaps[0].amount0In) > 1) {
+      action = 'buyEth'
+    } else if(Number(swaps[0].amount0Out) > 1) {
+      action = 'sellEth'
+    } else {
+      throw new Error('Should not happen')
+    }
+    console.log(`action ---> : ${action}`)
+    //console.log(`prices : ${JSON.stringify(prices, null, 2)}`)
+    
   })
   console.log(res.rows[0].now) // Hello world!
   //console.log(`res.rows ---> : ${res.rows[0].message}`)
---
+++ theGraph.js
@@ -0,0 +1,43 @@
+const axios = require('axios')
+async function fetchSwapForPair(pairId, timestamp) {
+  console.log(`pairId ---> : ${pairId}`)
+  //, timestamp: "1616946948" greater thans
+  const query = `
+  query GetSwap {
+    swaps(first: 1, orderBy: timestamp, orderDirection: desc, where:
+      { pair: "${pairId}", timestamp_gt: "${timestamp}" }
+     ) {
+          pair {
+            token0 {
+              symbol
+            }
+            token1 {
+              symbol
+            }
+          }
+          amount0In
+          amount0Out
+          amount1In
+          amount1Out
+          amountUSD
+          to
+      }
+  }
+  `
+  console.log(`query ---> : ${query}`)
+  const response = await axios({
+    url: 'https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2',
+    method: 'post',
+    data: {
+      query
+    }
+  })
+
+  console.log(`response.data : ${JSON.stringify(response.data, null, 2)}`)
+  return response.data.data.swaps
+  
+}
+
+module.exports = {
+  fetchSwapForPair
+}
