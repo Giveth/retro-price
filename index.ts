800c994e809d5800a1fb481efe8017426f5af32f
James Farrell <info@jamespfarrell.com>
2021-04-06 15:01:08 +0200

    query all

--- index.ts
+++ index.ts
@@ -89,7 +89,7 @@ async function run () {
   await client.connect()
 
   const res = await client.query(
-    `select d.id, d.amount, d.currency, d."createdAt" , EXTRACT(EPOCH from d."createdAt") as "donatedTime" from donation d where d.id = 64`
+    `select d.id, d.amount, d.currency, d."createdAt" , EXTRACT(EPOCH from d."createdAt") as "donatedTime" from donation d`
   )
 
   await processDonations(res.rows)
